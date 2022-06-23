10a3f2e842728d3a3573e5af739fa3a2b46276de
KHUSHBOO <50000046@mahindra.com>
2022-06-23 15:44:15 +0530

    mocha test cases

--- template/index.js
+++ template/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var nunjucks = require('nunjucks');
+var converter = require("./app/converter");
 
 var app =express();
+
 
 var env = nunjucks.configure(['views/'], {
     autoescape: true,
@@ -16,6 +18,24 @@ app.get('/', function(req, res) {
     }
     res.send(200, JSON.stringify(data))
 })
+
+app.get("/rgbToHex", function(req, res) {
+
+    var red   = parseInt(req.query.red, 10);
+    var green = parseInt(req.query.green, 10);
+    var blue  = parseInt(req.query.blue, 10);
+    var hex = converter.rgbToHex(red, green, blue);
+    res.send(hex);
+  
+  });
+  
+  app.get("/hexToRgb", function(req, res) {
+  
+    var hex = req.query.hex;
+    var rgb = converter.hexToRgb(hex);
+    res.send(JSON.stringify(rgb));
+  
+  });
 
 app.listen(4000, function() {
     console.log('templete app is running on 4000 port')
